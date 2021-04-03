f893f0e39a19bca5324e6ff1d191e5e7b4d4599d
egartley <egartley@users.noreply.github.com>
2021-04-03 19:21:38 -0400

    Add wind chime animations, idling

--- src/net/egartley/beyondorigins/entities/WindChimes.java
+++ src/net/egartley/beyondorigins/entities/WindChimes.java
@@ -7,24 +7,48 @@ import net.egartley.beyondorigins.core.logic.collision.Collisions;
 import net.egartley.beyondorigins.core.logic.collision.EntityEntityCollision;
 import net.egartley.beyondorigins.core.logic.interaction.BoundaryPadding;
 import net.egartley.beyondorigins.core.logic.interaction.EntityBoundary;
+import net.egartley.beyondorigins.core.threads.DelayedEvent;
 import net.egartley.beyondorigins.data.Images;
 import org.newdawn.slick.Animation;
 
 public class WindChimes extends AnimatedEntity {
 
-    private final int ANIMATION_THRESHOLD = 1200;
+    // private boolean isIdle = true;
+    private boolean swingAgain = true;
+
+    private final int ANIMATION_THRESHOLD = 200;
+    private final byte LEFT_SWING_ANIMATION = 0, RIGHT_SWING_ANIMATION = 1;
 
     public WindChimes(int x, int y) {
-        super("WindChimes", new SpriteSheet(Images.get(Images.WIND_CHIMES), 42, 54, 1, 3));
+        super("WindChimes", new SpriteSheet(Images.get(Images.WIND_CHIMES), 42, 54, 2, 5));
         isSectorSpecific = true;
         isDualRendered = false;
         setPosition(x, y);
     }
 
     @Override
+    public void tick() {
+        if (animation.isStopped() && swingAgain) {
+            swingAgain = false;
+            new DelayedEvent(0.1D * Util.randomInt(4, 8, true)) {
+                @Override
+                public void onFinish() {
+                    animation = animations.get(Util.fiftyFifty() ? LEFT_SWING_ANIMATION : RIGHT_SWING_ANIMATION);
+                    animation.restart();
+                    swingAgain = true;
+                }
+            }.start();
+        }
+    }
+
+    @Override
     public void setAnimations() {
-        animations.add(new Animation(Util.getAnimationFrames(sprites.get(0)), ANIMATION_THRESHOLD));
-        animation = animations.get(0);
+        animations.add(new Animation(Util.getAnimationFrames(sprites.get(LEFT_SWING_ANIMATION)), ANIMATION_THRESHOLD + Util.randomInt(10, 100, true)));
+        animations.add(new Animation(Util.getAnimationFrames(sprites.get(RIGHT_SWING_ANIMATION)), ANIMATION_THRESHOLD + Util.randomInt(10, 100, true)));
+        animation = animations.get(LEFT_SWING_ANIMATION);
+        for (Animation a : animations) {
+            a.setLooping(false);
+        }
     }
 
     @Override
