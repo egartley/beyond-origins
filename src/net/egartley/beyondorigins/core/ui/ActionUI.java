714b777880bffc401ff3db5b01fe3149117e3e5c
egartley <egartley@users.noreply.github.com>
2021-09-25 17:52:59 -0400

    Update Action UI

--- src/net/egartley/beyondorigins/core/ui/ActionUI.java
+++ src/net/egartley/beyondorigins/core/ui/ActionUI.java
@@ -9,9 +9,10 @@ public class ActionUI extends UIElement {
 
     private int textWidth;
     private int fullWidth;
+    private int iconWidth;
     private int playerMidX;
-    private int textX;
-    private final int PADDING = 6;
+    private int startX;
+    private final int PADDING = 4;
 
     private Color textColor = Color.white;
     private Color backgroundColor = new Color(0, 0, 0, 128);
@@ -23,24 +24,25 @@ public class ActionUI extends UIElement {
         this.text = text;
         icon = Images.getImageFromPath("resources/images/ui/keys/" + keycode + ".png");
         textWidth = font.getWidth(text);
-        fullWidth = textWidth + PADDING + icon.getWidth();
+        iconWidth = icon.getWidth();
+        fullWidth = textWidth + PADDING + iconWidth;
     }
 
     @Override
     public void render(Graphics graphics) {
+        graphics.drawImage(icon, startX, y - 19);
         graphics.setColor(backgroundColor);
-        graphics.fillRoundRect(textX - 2, y - 18, textWidth + 5, 16, 5);
+        graphics.fillRoundRect(startX + iconWidth + PADDING, y - 18, textWidth + 5, 16, 5);
         graphics.setColor(textColor);
         graphics.setFont(font);
-        graphics.drawString(text, textX, y - 16);
-        graphics.drawImage(icon, textX + textWidth + PADDING, y - 19);
+        graphics.drawString(text, startX + iconWidth + PADDING + 2, y - 16);
     }
 
     @Override
     public void tick() {
         y = Entities.PLAYER.y;
         playerMidX = Entities.PLAYER.x + (Entities.PLAYER.sprite.width / 2);
-        textX = playerMidX - (fullWidth / 2);
+        startX = playerMidX - (fullWidth / 2);
     }
 
 }
