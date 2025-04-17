2e9a20e5b5f2b1b25b7824bae6513afa13e06fdf
egartley <evanmgartley@gmail.com>
2025-04-17 00:33:44 -0400

    Improve camera tick

--- src/engine/camera.py
+++ src/engine/camera.py
@@ -17,28 +17,14 @@ class LevelCamera:
         bound_right = (self.view_x + self.sw) >= self.lw
         bound_top = self.view_y <= 0
         bound_bottom = (self.view_y + self.sh) >= self.lh
+        move_x = cx and ((self.player.left and not bound_left) or (self.player.right and not bound_right))
+        move_y = cy and ((self.player.up and not bound_top) or (self.player.down and not bound_bottom))
 
-        move_x, move_y = cx, cy
-        if cx:
-            if self.player.left:
-                move_x = not bound_left
-            if self.player.right:
-                move_x = not bound_right
-            if not cy and (self.player.up or self.player.down):
-                move_y = False
-        if cy:
-            if self.player.up:
-                move_y = not bound_top
-            if self.player.down:
-                move_y = not bound_bottom
-            if not cx and (self.player.left or self.player.right):
-                move_x = False
         if not cx and not cy:
             if self.player.up or self.player.down:
                 move_y = False
             if self.player.left or self.player.right:
                 move_x = False
-
         if move_x:
             self.view_x += cc_x
         else:
@@ -48,11 +34,5 @@ class LevelCamera:
         else:
             self.player.move_y(delta)
 
-        if self.view_x < 0:
-            self.view_x = 0
-        if self.view_x + self.sw > self.lw:
-            self.view_x = self.lw - self.sw
-        if self.view_y < 0:
-            self.view_y = 0
-        if self.view_y + self.sh > self.lh:
-            self.view_y = self.lh - self.sh
+        self.view_x = max(0, min(self.view_x, self.lw - self.sw))
+        self.view_y = max(0, min(self.view_y, self.lh - self.sh))
