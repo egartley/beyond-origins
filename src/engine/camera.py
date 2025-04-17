6491d450dc12d55051ecf4ef28efd0b664051ee6
egartley <evanmgartley@gmail.com>
2025-04-17 01:24:45 -0400

    Improve camera tick

--- src/engine/camera.py
+++ src/engine/camera.py
@@ -5,54 +5,46 @@ class LevelCamera:
 
     def __init__(self, player: LevelEntity, lw: int, lh: int, sw: int, sh: int):
         self.player = player
-        self.lw, self.lh, self.sw, self.sh = lw, lh, sw, sh
+        self.level_width, self.level_height = lw, lh
+        self.screen_width, self.screen_height = sw, sh
         self.view_x, self.view_y = 0, 0
 
     def tick(self, delta: float):
-        cc_x = self.player.vel_x * delta
-        cc_y = self.player.vel_y * delta
-        cx = abs(self.player.rect.centerx - (self.sw // 2)) <= abs(cc_x)
-        cy = abs(self.player.rect.centery - (self.sh // 2)) <= abs(cc_y)
-        bound_left = self.view_x <= 0
-        bound_right = (self.view_x + self.sw) >= self.lw
-        bound_top = self.view_y <= 0
-        bound_bottom = (self.view_y + self.sh) >= self.lh
+        change_x, change_y = self.player.vel_x * delta, self.player.vel_y * delta
+        player_center_x = abs(self.player.rect.centerx - (self.screen_width // 2)) <= abs(change_x)
+        player_center_y = abs(self.player.rect.centery - (self.screen_height // 2)) <= abs(change_y)
 
-        move_x, move_y = cx, cy
-        if cx:
+        move_x, move_y = player_center_x, player_center_y
+        if player_center_x:
             if self.player.left:
-                move_x = not bound_left
+                move_x = self.view_x > 0
             if self.player.right:
-                move_x = not bound_right
-            if not cy and (self.player.up or self.player.down):
+                move_x = (self.view_x + self.screen_width) < self.level_width
+            if not player_center_y and (self.player.up or self.player.down):
                 move_y = False
-        if cy:
+        if player_center_y:
             if self.player.up:
-                move_y = not bound_top
+                move_y = self.view_y > 0
             if self.player.down:
-                move_y = not bound_bottom
-            if not cx and (self.player.left or self.player.right):
+                move_y = (self.view_y + self.screen_height) < self.level_height
+            if not player_center_x and (self.player.left or self.player.right):
                 move_x = False
-        if not cx and not cy:
+        if not player_center_x and not player_center_y:
             if self.player.up or self.player.down:
                 move_y = False
             if self.player.left or self.player.right:
                 move_x = False
 
         if move_x:
-            self.view_x += cc_x
+            self.view_x = max(self.view_x + change_x, 0)
+            if self.view_x + self.screen_width > self.level_width:
+                self.view_x = self.level_width - self.screen_width
         else:
             self.player.move_x(delta)
+
         if move_y:
-            self.view_y += cc_y
+            self.view_y = max(self.view_y + change_y, 0)
+            if self.view_y + self.screen_height > self.level_height:
+                self.view_y = self.level_height - self.screen_height
         else:
             self.player.move_y(delta)
-
-        if self.view_x < 0:
-            self.view_x = 0
-        if self.view_x + self.sw > self.lw:
-            self.view_x = self.lw - self.sw
-        if self.view_y < 0:
-            self.view_y = 0
-        if self.view_y + self.sh > self.lh:
-            self.view_y = self.lh - self.sh
